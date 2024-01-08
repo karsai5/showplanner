88a0d112bd5b85a13509ec044ebe25b721cd6d14
Linus Karsai <linus@linusk.com.au>
2024-01-09 09:42:17 +1100

    Fix linting

--- packages/frontend/config/appInfo.ts
+++ packages/frontend/config/appInfo.ts
@@ -4,8 +4,8 @@ export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Showplanner",
-  apiDomain: SUPERTOKENS_URL,
-  websiteDomain: FRONTEND_URL,
+  apiDomain: SUPERTOKENS_URL || "http://localhost:8080",
+  websiteDomain: FRONTEND_URL || "http://localhost:3000",
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
 };
