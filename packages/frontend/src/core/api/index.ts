9b9e09d42d8f42d84dc8471a197de13e0b614a49
Linus Karsai <linus@linusk.com.au>
2024-01-09 08:10:30 +1100

    Use api from environment variables

--- packages/frontend/src/core/api/index.ts
+++ packages/frontend/src/core/api/index.ts
@@ -1,5 +1,11 @@
-import { DefaultApi } from "./generated"
+import { Configuration, DefaultApi } from "./generated"
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const config = new Configuration({
+  basePath: API_URL,
+});
 
 export const getApi = (): DefaultApi => {
-  return new DefaultApi();
+  return new DefaultApi(config);
 }
