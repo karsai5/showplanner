48bf2a47a616affc3dcc616e55572861e8c5631a
Linus Karsai <linus@linusk.com.au>
2024-01-09 09:39:59 +1100

    Add extra envs

--- packages/frontend/config/appInfo.ts
+++ packages/frontend/config/appInfo.ts
@@ -1,8 +1,11 @@
+export const SUPERTOKENS_URL = process.env.NEXT_PUBLIC_SUPERTOKENS_URL;
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
+
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Showplanner",
-  apiDomain: "http://localhost:8080",
-  websiteDomain: "http://localhost:3000",
+  apiDomain: SUPERTOKENS_URL,
+  websiteDomain: FRONTEND_URL,
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
 };
