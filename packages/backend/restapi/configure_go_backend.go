c680a52075819b8a512ad5d4f308bc3d7f5a61be
Linus Karsai <linus@linusk.com.au>
2024-01-14 12:35:27 +1100

    Improve middleware

--- packages/backend/restapi/configure_go_backend.go
+++ packages/backend/restapi/configure_go_backend.go
@@ -4,18 +4,13 @@ package restapi
 
 import (
 	"crypto/tls"
-	"log"
-	"regexp"
-	"strings"
-	"time"
 
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
-	"github.com/supertokens/supertokens-golang/recipe/session"
 
 	"go-backend/domains/permissions"
+	"go-backend/restapi/middleware"
 	"go-backend/restapi/operations"
-	"go-backend/utils"
 	"net/http"
 
 	"github.com/supertokens/supertokens-golang/supertokens"
@@ -67,55 +62,14 @@ func configureServer(s *http.Server, scheme, addr string) {
 // The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
 // The middleware executes after routing but before authentication, binding and validation.
 func setupMiddlewares(handler http.Handler) http.Handler {
-	return VerifiedEndpointHandler(handler)
-}
-
-func VerifiedEndpointHandler(handlerToWrap http.Handler) *VerifiedEndpoint {
-	return &VerifiedEndpoint{handlerToWrap}
-}
-
-type VerifiedEndpoint struct {
-	handler http.Handler
-}
-
-// ServeHTTP handles the request by passing it to the real
-// handler and logging the request details
-func (ve *VerifiedEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	publicEndpoint, err := regexp.Match(`^/v1/public.*$`, []byte(r.URL.Path))
-	if err != nil {
-		panic("Error with regex: " + err.Error())
-	}
-
-	if !publicEndpoint {
-		session.VerifySession(nil, ve.handler.ServeHTTP).ServeHTTP(w, r)
-	} else {
-		ve.handler.ServeHTTP(w, r)
-	}
-
-	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+	return middleware.VerifiedEndpointHandler(handler)
 }
 
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
 	// return handler
-	return corsMiddleware(supertokens.Middleware(handler))
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, r *http.Request) {
-		writer.Header().Set("Access-Control-Allow-Origin", utils.GetEnvVariable("FRONTEND_URL", true))
-		writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		if r.Method == "OPTIONS" {
-			// we add content-type + other headers used by SuperTokens
-			writer.Header().Set("Access-Control-Allow-Headers",
-				strings.Join(append([]string{"Content-Type"},
-					supertokens.GetAllCORSHeaders()...), ","))
-			writer.Write([]byte(""))
-		} else {
-			next.ServeHTTP(writer, r)
-		}
-	})
+	return middleware.LogsMiddleware(
+		middleware.CorsMiddleware(
+			supertokens.Middleware(handler)))
 }
---
+++ packages/backend/restapi/middleware/cors-middleware.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"go-backend/utils"
+	"net/http"
+	"strings"
+
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, r *http.Request) {
+		writer.Header().Set("Access-Control-Allow-Origin", utils.GetEnvVariable("FRONTEND_URL", true))
+		writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		if r.Method == "OPTIONS" {
+			// we add content-type + other headers used by SuperTokens
+			writer.Header().Set("Access-Control-Allow-Headers",
+				strings.Join(append([]string{"Content-Type"},
+					supertokens.GetAllCORSHeaders()...), ","))
+			writer.Write([]byte(""))
+		} else {
+			next.ServeHTTP(writer, r)
+		}
+	})
+}
