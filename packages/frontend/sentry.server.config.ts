a3af5b509a7fed374ee7a167ef57360951d88b83
Linus Karsai <linus@linusk.com.au>
2024-01-14 13:06:51 +1100

    Add sentry

---
+++ packages/frontend/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5697b7a90272e7e3bb87451be4d42372@o410369.ingest.sentry.io/4506567153156096",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
