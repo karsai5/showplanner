926d1ecf48f45d1679d068e7b954681764ef9c5e
Linus Karsai <linus@linusk.com.au>
2024-01-19 15:35:56 +1100

    Fix import order on 404 page

--- packages/frontend/src/pages/404/index.tsx
+++ packages/frontend/src/pages/404/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
 import imageSrc from './404.jpg';
+
 const NotFoundPage = () => {
   return <div className="prose">
     <h1>Page not found</h1>
-    <p>Do you ever get the feeling you're not standing in your light. Well this is one of those moments! Unfotunately this page doesn't exist, but I'm sure if you keep searching you'll find what you're looking for.</p>
+    <p>Do you ever get the feeling you&apos;re not standing in your light. Well this is one of those moments! Unfotunately this page doesn&apos;t exist, but I&apos;m sure if you keep searching you&apos;ll find what you&apos;re looking for.</p>
     <Link href="https://q2qcomics.com/">
-    <Image src={imageSrc} alt="Cartoon from q2q"/>
+      <Image src={imageSrc} alt="Cartoon from q2q" />
     </Link>
   </div>
 }
