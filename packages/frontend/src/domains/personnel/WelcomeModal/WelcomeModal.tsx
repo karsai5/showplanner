99e6103dd6a6ae0cd9b77f565f11c0eca4345f57
Linus Karsai <linus@linusk.com.au>
2024-01-24 07:40:03 +1100

    Made weclome modal smaller and fixed spelling mistake

--- packages/frontend/src/domains/personnel/WelcomeModal/WelcomeModal.tsx
+++ packages/frontend/src/domains/personnel/WelcomeModal/WelcomeModal.tsx
@@ -10,14 +10,14 @@ const WelcomeModal = () => {
     <div className={cc("modal", { ["modal-open"]: isOpen })}>
       <div className="modal-box">
         <h3 className="font-bold text-lg">Welcome to the ShowPlanner!</h3>
-        <Image src={heroImage} alt=""/>
+        <Image height="200" className="mx-auto" src={heroImage} alt=""/>
         <p className="py-4">
           Hi there, thanks for signing up to use the ShowPlanner! Before you can
           acess the site we just need to setup the rest of your profile.
         </p>
         <p className="py-4">
           The information here will get shared with the stage manager of shows
-          you join. In instances where other people are helping with organing
+          you join. In instances where other people are helping with organising
           the show, your information will also be available to them.
         </p>
         <div className="modal-action">
