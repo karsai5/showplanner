80daed5599bb48a74337a6fe8f431334f44ef804
Linus Karsai <linus@linusk.com.au>
2024-02-02 15:38:05 +1100

    Remove comments

--- packages/backend/pkg/notifications/availabilities_mailbox/mailbox.go
+++ packages/backend/pkg/notifications/availabilities_mailbox/mailbox.go
@@ -16,23 +16,17 @@ var debounced = helpers.CreateDebouncer(20 * time.Minute)
 func Setup() {
 	router := postoffice.CreateNewRouter()
 
-	// shutdown when SIGTERM is recieved
 	router.AddPlugin(plugin.SignalsHandler)
-	// Router level middleware are executed for every message sent to the router
+
 	router.AddMiddleware(
-		// CorrelationID will copy the correlation id from the incoming message's metadata to the produced messages
 		middleware.CorrelationID,
 
-		// The handler function is retried if it returns an error.
-		// After MaxRetries, the message is Nacked and it's up to the PubSub to resend it.
 		middleware.Retry{
 			MaxRetries:      3,
 			InitialInterval: time.Millisecond * 100,
 			Logger:          postoffice.RouterLogger,
 		}.Middleware,
 
-		// Recoverer handles panics from handlers.
-		// In this case, it passes them as errors to the Retry middleware.
 		middleware.Recoverer,
 	)
 
